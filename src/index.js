2ef1593fba7ba2204bbdd135357d1a0c9fdc8b64
Chuks Alimele <chuksalimele@gmail.com>
2021-08-18 14:49:46 +0100

    Fixed production build

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@
 */
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 // core components
 import Portal from "layouts/Portal.js";
