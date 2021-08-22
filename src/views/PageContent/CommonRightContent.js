84a7b8db7932d90acfe62d50a11e29047db652c0
Chuks Alimele <chuksalimele@gmail.com>
2021-08-22 23:23:30 +0100

    Link dashboard to views

--- src/views/PageContent/CommonRightContent.js
+++ src/views/PageContent/CommonRightContent.js
@@ -11,11 +11,11 @@ import CardHeader from "components/Card/CardHeader.js";
 import CardIcon from "components/Card/CardIcon.js";
 import CardFooter from "components/Card/CardFooter.js";
 import CardBody from "components/Card/CardBody.js";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import FormHelperText from "@material-ui/core/FormHelperText";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
+//import InputLabel from "@material-ui/core/InputLabel";
+//import MenuItem from "@material-ui/core/MenuItem";
+//import FormHelperText from "@material-ui/core/FormHelperText";
+//import FormControl from "@material-ui/core/FormControl";
+//import Select from "@material-ui/core/Select";
 import Grid from "@material-ui/core/Grid";
 
 import styles from "assets/jss/material-dashboard-react/views/dashboardStyle.js";
@@ -25,7 +25,9 @@ import Success from "components/Typography/Success";
 const useStyles = makeStyles(styles);
 const AccountBalance = "36,000";
 const AccountCurrency = "USD";
+const CustomerID = "2873739";
 const LastWithdrawalOrDepositDateTime = "Last Deposit: 4th June 2021 13:23";
+const AccountCreatedDateTime = "Created: 23rd August 2018";
 
 const itemStyle = {
   width: "100%",
@@ -33,42 +35,34 @@ const itemStyle = {
 
 export default function CommonRightContent() {
   const classes = useStyles();
-  const [selectedAccount, setSelectedAccount] = React.useState("");
+  //const [selectedAccount, setSelectedAccount] = React.useState("");
 
-  const handleChangeAccount = (event) => {
+  /*const handleChangeAccount = (event) => {
     setSelectedAccount(event.target.value);
   };
-
+*/
   return (
     <Grid container item spacing={2}>
       <Grid item spacing={2} style={itemStyle}>
         <Card>
           <CardHeader color="success">
-            <h4 style={{ margin: 0 }}>Choose Account</h4>
+            <h4 style={{ margin: 0 }}>
+              ID: <span>{CustomerID}</span>
+            </h4>
           </CardHeader>
-          <CardBody>
-            <FormControl className={classes.formControl} style={itemStyle}>
-              <InputLabel id="select-account-label">
-                {AccountCurrency}
-              </InputLabel>
-              <Select
-                style={itemStyle}
-                labelId="select-account-label"
-                id="select-account"
-                value={selectedAccount}
-                onChange={handleChangeAccount}
-                renderValue={(value) => `${value}`}
-              >
-                <MenuItem value="">
-                  <em>None</em>
-                </MenuItem>
-                <MenuItem value={1111}>1111</MenuItem>
-                <MenuItem value={44444}>44444</MenuItem>
-                <MenuItem value={8888}>8888</MenuItem>
-              </Select>
-              <FormHelperText>Created: 23rd August 2018</FormHelperText>
-            </FormControl>
+          <CardBody style={{ paddingTop: "10px", paddingBottom: 0 }}>
+            <h3 className={classes.cardTitle}>
+              <small>{AccountCurrency}</small>
+            </h3>
           </CardBody>
+          <CardFooter stats>
+            <div className={classes.stats}>
+              <Success>
+                <InfoRounded />
+              </Success>
+              <p>{AccountCreatedDateTime}</p>
+            </div>
+          </CardFooter>
         </Card>
       </Grid>
       <Grid item spacing={2} style={itemStyle}>
